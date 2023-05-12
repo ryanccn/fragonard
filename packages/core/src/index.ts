6ee6ed58f3293b65a8e4fb401f8bffd6b12f82c5
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-05-12 22:21:46 +0800

    chore: export more types from global scope

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,19 @@
 export { Client } from "~/client";
+
 export { defineLayer, Layer } from "~/layer";
-export { LayerCommandType } from "~/layer/commands";
+export {
+	LayerCommandType,
+	LayerCommand,
+	LayerContextMenuCommand,
+	LayerSlashCommand,
+} from "~/layer/commands";
+export { LayerButton } from "~/layer/buttons";
+export {
+	EventListener,
+	EventListenerSupportedEvents,
+	LayerListener,
+} from "~/layer/listeners";
 export { BaseContext } from "~/layer/baseContext";
+
 export { Context } from "~/context";
+export { Logger } from "~/logger";
