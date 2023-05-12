4ea5ec9510425907904ece387b069d534081c548
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-05-12 09:35:28 +0800

    feat: add basic layer-redis

---
+++ packages/layer-redis/src/index.ts
@@ -0,0 +1,23 @@
+import { defineLayer } from "@fragonard/core";
+import { createClient } from "redis";
+
+const _client = createClient({ url: process.env.REDIS_URL });
+_client.on("error", (err) => {
+	console.error(err);
+});
+const client = _client.connect().then(() => _client);
+
+export default defineLayer({
+	id: "official-layer-redis",
+	api: {
+		getClient() {
+			return client;
+		},
+		async get(k: string) {
+			return await (await client).get(k);
+		},
+		async set(k: string, v: string | number) {
+			await (await client).set(k, v);
+		},
+	},
+});
